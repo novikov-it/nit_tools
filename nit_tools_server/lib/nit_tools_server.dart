ff2bb153e4a4fd591aeb67e4956a07d50d96f772
Evgenii Novikov <disregard.the.rest@gmail.com>
2024-12-13 11:01:48 +0300

    export api_response for custom server endpoints

--- nit_tools_server/lib/nit_tools_server.dart
+++ nit_tools_server/lib/nit_tools_server.dart
@@ -4,6 +4,7 @@ export 'src/generated/endpoints.dart';
 export 'src/generated/protocol.dart';
 
 export 'src/extra_classes/object_wrapper.dart';
+export 'src/extra_classes/api_response.dart';
 export 'src/endpoints/crud_endpoint.dart';
 export 'src/configuration/crud_config.dart';
 export 'src/configuration/get_one_by_id.dart';
