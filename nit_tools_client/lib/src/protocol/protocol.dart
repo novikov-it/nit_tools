7bae0e73a2fe411093b43560dc6648f65e37022e
Evgenii Novikov <disregard.the.rest@gmail.com>
2025-04-08 09:19:08 +0300

    after merge generation

--- nit_tools_client/lib/src/protocol/protocol.dart
+++ nit_tools_client/lib/src/protocol/protocol.dart
@@ -10,7 +10,15 @@
 
 library protocol; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
+import 'package:nit_tools_client/src/extra_classes/nit_backend_filter.dart'
+    as _i12;
+import 'package:nit_tools_client/src/extra_classes/object_wrapper.dart' as _i13;
+import 'package:serverpod_auth_client/serverpod_auth_client.dart' as _i16;
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
+
+import '/src/extra_classes/api_response.dart' as _i14;
+import '/src/extra_classes/nit_backend_filter.dart' as _i15;
+import '/src/extra_classes/object_wrapper.dart' as _i11;
 import 'chats/chat_channel.dart' as _i2;
 import 'chats/chat_initial_data.dart' as _i3;
 import 'chats/chat_message.dart' as _i4;
@@ -20,22 +28,16 @@ import 'media/nit_media_type.dart' as _i7;
 import 'nit_app_notification.dart' as _i8;
 import 'nit_fcm_token.dart' as _i9;
 import 'protocol.dart' as _i10;
-import '/src/extra_classes/object_wrapper.dart' as _i11;
-import 'package:nit_tools_client/src/extra_classes/nit_backend_filter.dart'
-    as _i12;
-import 'package:nit_tools_client/src/extra_classes/object_wrapper.dart' as _i13;
-import '/src/extra_classes/api_response.dart' as _i14;
-import '/src/extra_classes/nit_backend_filter.dart' as _i15;
-import 'package:serverpod_auth_client/serverpod_auth_client.dart' as _i16;
+
 export 'chats/chat_channel.dart';
 export 'chats/chat_initial_data.dart';
 export 'chats/chat_message.dart';
 export 'chats/chat_participant.dart';
+export 'client.dart';
 export 'media/nit_media.dart';
 export 'media/nit_media_type.dart';
 export 'nit_app_notification.dart';
 export 'nit_fcm_token.dart';
-export 'client.dart';
 
 class Protocol extends _i1.SerializationManager {
   Protocol._();
@@ -59,7 +61,6 @@ class Protocol extends _i1.SerializationManager {
           _i14.ApiResponse.manualDeserialization<T>(data);
       if (manualDeserialization != null) return manualDeserialization;
     }
-
     if (t == _i2.NitChatChannel) {
       return _i2.NitChatChannel.fromJson(data) as T;
     }
